ee664c76dd5655ba8e1f2ad4beb481345a393994
KNJ <knj@wazly.net>
2014-09-25 16:16:48 +0900

    Switch to development

--- ppc.js
+++ ppc.js
@@ -65,11 +65,11 @@ var ppc = {}, base = {};
 // 管理者設定 (static)
 ppc.admin = gs(base, {
 	version: gs(base, {
-		script: '140920',
+		script: '*',
 		css: '140920',
 	}),
 	canonical_domain: 'eshies.net',
-	domain: 'eshies.net',
+	domain: 'dev.eshies.net',
 	max_illusts: 28, // 測定対象上限数
 	min_illusts: 20, // 測定対象下限数
 	suspended: false,
