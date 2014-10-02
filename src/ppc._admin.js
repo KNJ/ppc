a0cd2d98bcbf1ed2e364c04c030cda895e71eaa6
KNJ <knj@wazly.net>
2014-10-03 02:03:28 +0900

    Switch to development

--- src/ppc._admin.js
+++ src/ppc._admin.js
@@ -3,11 +3,11 @@
 // 管理者設定 (static)
 ppc.admin = cloz(base, {
 	version: cloz(base, {
-		script: '141001',
+		script: '*',
 		css: '140926',
 	}),
 	canonical_domain: 'eshies.net',
-	domain: 'eshies.net',
+	domain: 'dev.eshies.net',
 	max_illusts: 28, // 測定対象上限数
 	min_illusts: 20, // 測定対象下限数
 	suspended: false,
