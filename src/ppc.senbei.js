255baedbe6935f9ec4f0d6a316176b0ff2d7c729
KNJ <knj@wazly.net>
2014-10-13 00:58:40 +0900

    Update Senbei

--- src/ppc.senbei.js
+++ src/ppc.senbei.js
@@ -5,19 +5,53 @@
 		self: null,
 		init: function(){
 			var self = senbei({
-				name: ppc.user.get('id'),
-				use: 'localStorage',
-				write: ['localStorage', function(s){ppc.storage = s;}]
+				name: 'ppc' + ppc.user.get('id'),
+				read: 'localStorage',
+				write: 'localStorage',
+				// write: ['localStorage', function(s){ppc.storage = s;}]
 			},[
 				{
-					base: $('.senbei'),
-					n: function(){
-						console.log(ppc.storage);
+					base: $('#gstchk'),
+					condition: function(b){ return b.prop('checked'); },
+					t: function(){ ppc.user.set('guest', true); },
+					f: function(){ ppc.user.set('guest', false); },
+					on: {
+						change: true,
+					},
+				},
+				{
+					base: $('#rnkchk'),
+					condition: function(b){ return b.prop('checked'); },
+					t: function(){ ppc.user.set('vranking', true); },
+					f: function(){ ppc.user.set('vranking', false); },
+					on: {
+						change: true,
+					},
+				},
+				{
+					base: $('#check-enable-animation'),
+					condition: function(b){ return b.prop('checked'); },
+					t: function(){ ppc.user.set('animation', true); },
+					f: function(){ ppc.user.set('animation', false); },
+					on: {
+						change: true,
+					},
+				},
+				{
+					base: $('#ppcranking'),
+					condition: function(b){ return b.prop('checked'); },
+					t: function(){
+						$('#login_status .join').text('参加');
+						ppc.user.set('pranking', true);
+					},
+					f: function(){
+						$('#login_status .join').text('不参加（「環境設定」から変更できます）');
+						ppc.user.set('pranking', false);
 					},
 					on: {
 						change: true,
 					},
-				}
+				},
 			]);
 			this.set('self', self);
 		},
